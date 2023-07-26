427a87522d182e348b6b49cd7a7a5a271128639c
Diego Silva <dssilva0710@hotmail.com>
2023-07-26 12:45:21 -0300

    atividade resolvida mo8

---
+++ exercicio_mod8/src/br/com/diego/Main.java
@@ -0,0 +1,9 @@
+package br.com.diego;
+
+public class Main {
+
+    public static void main(String args[]) {
+        MediaAritmetica nota1 = new MediaAritmetica();
+        nota1.media();
+    }
+}
---
+++ exercicio_mod8/src/br/com/diego/MediaAritmetica.java
@@ -0,0 +1,15 @@
+package br.com.diego;
+
+public class MediaAritmetica {
+
+    public void media() {
+
+        double nota1 = 10;
+        double nota2 = 8;
+        double nota3 = 6;
+        double nota4 = 5;
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+        System.out.println("A media das notas é: " + media);
+    }
+}
